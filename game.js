59af252cd25a2bb2d26e687293840f4eb793e4ec
Heather Knight <heather.knight.pdx@gmail.com>
2017-08-14 16:01:22 -0700

    DRYd my code.

--- game.js
+++ game.js
@@ -1,96 +1,49 @@
 var message="";
 var score = 0;
 
+function askQuestion(question, answer, correctMessage, wrongMessage, location){
+  var response = prompt(question);
+  if(response.toLowerCase()==answer.toLowerCase()){
+    message = "<img src=img/check-mark-8-128.jpg>"+correctMessage;
+    score += 1;
+  } else {
+    message = "<img src=img/x-mark-128.jpg>"+wrongMessage;
+  }
+  document.getElementById(location).innerHTML = message;
+}
+
 function guessingGame(){
-myTravel();
-myBeverage();
-myVehicle();
-myAge();
-myWebsite();
-myHobby();
-myScore();
+  askQuestion("Do I like to travel?", "yes", "Absolutely!  I love to travel.", "You don't know me!", 'answerOne');
+  askQuestion("Do I like coffee?", "yes", "You know me so well!", "Wrong! Coffee is sacred in Portland!", 'answerTwo');
+  askQuestion("Have I ridden my 1980 Vespa P200 across the US?", "yes", "Yes, I did. In 2006.  Over 10 days.", "Wrong Answer! I am an OG Cannonballer.", 'answerThree');
+  askQuestion("Did a website I create go viral recently?", "yes", "Yes, it was a dating website I created for myself.", "That would be no.  I'm a celebrity!", 'answerFour');
+  askQuestion("Do I like to hike and backpack?", "yes", "Yes, I like to hike and backpack in Glacier National Park!", "I don't think you know me at all.  Nature is life.", 'answerFive');
+  myAge();
+  myScore();
 }
 
-function updatePage(elementId) {
-  document.getElementById(elementId).innerHTML = message;
-}
-//question one
-  function myTravel(){
-    var travelStatus = prompt("Do I like to travel?");
-    if(travelStatus == "yes" || travelStatus == "Yes"|| travelStatus == "YES"){
-      message = "Absolutely!  I love to travel.";
-      score += 1;
-    } else {
-      message = "You don't know me!";
-    }
-    updatePage('answerOne');
-  }
-//question two
-  function myBeverage(){
-    var beverageStatus = prompt("Do I like coffee?");
-    if(beverageStatus == "yes" || beverageStatus == "Yes"){
-      message = "You know me so well!";
-      score += 1;
-    } else {
-      message = "Wrong!  Coffee is sacred in Portland!";
-    }
-    updatePage('answerTwo');
-  }
-//question three
-  function myVehicle(){
-    var vehicleStatus = prompt("Have I ridden my 1980 Vespa P200 across the US?");
-    if(vehicleStatus == "yes" || vehicleStatus == "Yes"){
-      message = "Yes, I did. In 2006.  Over 10 days.";
-      score += 1;
-    } else {
-      message = "Wrong Answer! I am an OG Cannonballer.";
-    }
-    updatePage('answerThree');
-  }
-//question four
-  function myWebsite(){
-    var websiteStatus = prompt("Did a website I create go viral recently?");
-    if (websiteStatus == "yes" || websiteStatus == "Yes"){
-      message = "Yes, it was a dating website I created for myself.";
-      score += 1;
-    } else {
-      message ="That would be no.  I'm a celebrity!";
-    }
-    updatePage('answerFour');
-  }
-//question five
-  function myHobby(){
-    var hobbyStatus = prompt("Do I like to hike and backpack?");
-    if (hobbyStatus == "yes" || hobbyStatus == "Yes"){
-      message ="Yes, I like to hike and backpack in Glacier National Park!"
-      score += 1;
-    } else {
-      message ="I don't think you know me at all.  Nature is life.";
-    }
-    updatePage('answerFive');
-  }
 //question six
-  function myAge(){
-    for (var i=0; i<1; i++){
-      var ageStatus = 43;
-      var guessAge = prompt("What is your best guess at my age?");
-      if(guessAge == ageStatus){
-        message =("You guessed my exact age!  Congrats!");
-        score += 1;
-      } else if (guessAge > ageStatus){
-        message =("Too old!");
-      } else if (guessAge < ageStatus){
-        message =("Too young!");
-      }
-      updatePage('answerSix');
+function myAge(){
+  for (var i=0; i<1; i++){
+    var ageStatus = 43;
+    var guessAge = prompt("What is your best guess at my age?");
+    if(guessAge == ageStatus){
+      message ="You guessed my exact age!  Congrats!";
+      score += 1;
+    } else if (guessAge > ageStatus){
+      message ="Too old!";
+    } else if (guessAge < ageStatus){
+      message ="Too young!";
     }
+    document.getElementById('answerSix').innerHTML = message;
   }
+}
 
-  function myScore(){
-    if (score === 6){
-    message =("Your score is " + score +".  You answered every question correctly.");
-    } else {
-    message =("Your score is "+ score +".  You did not answer all the questions correctly.");
-    }
-    updatePage('scoreAnswer');
+function myScore(){
+  if (score === 6){
+    message ="Your score is " + score +".  You answered every question correctly.";
+  } else {
+    message ="Your score is "+ score +".  You did not answer all the questions correctly.";
   }
+  document.getElementById('scoreAnswer').innerHTML = message;
+}
